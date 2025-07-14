b91ee448140d47c1d48e54cd109e3d719a906a1a
Johab Freitas <freitasjohab@gmail.com>
2025-07-14 20:03:27 -0300

    feat: Modelando os dados da aplicação, episódios e temporadas

--- src/main/java/br/com/johabfreitas/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/johabfreitas/screenmatch/ScreenmatchApplication.java
@@ -1,9 +1,6 @@
 package br.com.johabfreitas.screenmatch;
 
-import br.com.johabfreitas.screenmatch.model.DadosEpisodio;
-import br.com.johabfreitas.screenmatch.model.DadosSerie;
-import br.com.johabfreitas.screenmatch.service.ConsumoAPI;
-import br.com.johabfreitas.screenmatch.service.ConverteDados;
+import br.com.johabfreitas.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -18,15 +15,10 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoAPI = new ConsumoAPI();
-		var json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Season=1&apikey=c9d1a3fb");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
-		json = consumoAPI.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Season=1&episode=2&apikey=c9d1a3fb");
-		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
+
+		Principal principal = new Principal();
+		principal.exibeMenu();
+
 
 	}
 }
